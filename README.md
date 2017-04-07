d42a5fa54b758a883f67b111dafff74551ddd113
Michael Faille <michael.faille@desjardins.com>
2017-04-07 11:20:17 -0400

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+## Usage :
+
+Begin your function like this :
+
+```
+func Example(){
+	Enter()
+	defer Exit(time.Now())
+    (...)
+```
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"runtime"
+	"time"
+)
+
+func TimeTrack(start time.Time) time.Duration {
+	elapsed := time.Since(start)
+	return elapsed
+}
+
+func Enter() {
+	// Skip this function, and fetch the PC and file for its parent
+	pc, _, _, _ := runtime.Caller(1)
+	// Retrieve a Function object this functions parent
+	functionObject := runtime.FuncForPC(pc)
+	// Regex to extract just the function name (and not the module path)
+	extractFnName := regexp.MustCompile(`^.*\.(.*)$`)
+	fnName := extractFnName.ReplaceAllString(functionObject.Name(), "$1")
+	fmt.Printf("Entering %s\n", fnName)
+	log.Printf("Entering %s\n", fnName)
+}
+
+func Exit(start time.Time) {
+	// Skip this function, and fetch the PC and file for its parent
+	pc, _, _, _ := runtime.Caller(1)
+	// Retrieve a Function object this functions parent
+	functionObject := runtime.FuncForPC(pc)
+	// Regex to extract just the function name (and not the module path)
+	extractFnName := regexp.MustCompile(`^.*\.(.*)$`)
+	fnName := extractFnName.ReplaceAllString(functionObject.Name(), "$1")
+	fmt.Printf("Exiting  %s after %s \n", fnName, TimeTrack(start).String())
+	log.Printf("Exiting  %s after %s \n", fnName, TimeTrack(start).String())
+}
